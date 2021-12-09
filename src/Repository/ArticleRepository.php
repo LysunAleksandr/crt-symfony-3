f563d42a6a7f7d6f9364ebf3a0be6b3f59da37bb
LysunAleksandr <94159313+LysunAleksandr@users.noreply.github.com>
2021-12-09 16:04:52 +0500

    Create ArticleRepository.php

---
+++ src/Repository/ArticleRepository.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+/**
+ * @method Article|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Article|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Article[]    findAll()
+ * @method Article[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ArticleRepository extends ServiceEntityRepository
+{
+    public const PAGINATOR_PER_PAGE = 2;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Article::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy([], ['created_at' => 'ASC', 'title' => 'ASC']);
+    }
+
+    public function getArticlePaginator(int $offset): Paginator
+    {
+        $query = $this->createQueryBuilder('a')
+            ->orderBy('a.created_at', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
+    }
+
+
+    // /**
+    //  * @return Article[] Returns an array of Article objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Article
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
