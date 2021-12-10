74def027c8491f1ae149c4f6bd46a7ec8b94afe8
LysunAleksandr <94159313+LysunAleksandr@users.noreply.github.com>
2021-12-10 13:31:04 +0500

    Update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\StaticPage;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -37,7 +38,10 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
         yield MenuItem::linkToCrud('Article', 'fas fa-list', Article::class);
         yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class);
-        yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
+        if( $this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
+            yield MenuItem::linkToCrud('StaticPage', 'fas fa-list', StaticPage::class);
+        }
     }
 
 
